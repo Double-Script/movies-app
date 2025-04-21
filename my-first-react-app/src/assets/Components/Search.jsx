7aa152b836dd7d6d8083c80ca869a375265d8958
vedant patil <patilvedantraj@gmail.com>
2025-04-21 22:35:00 +0530

    first commit

---
+++ my-first-react-app/src/assets/Components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm}) => {
+  return (
+    <div>
+      <div className= "search">
+        <div>
+            <img src="./search.svg" alt="search" />
+            <input type="text"
+                placeholder='Search Through thousand of movies'
+                value={searchTerm}
+                onChange={(event)=>setSearchTerm(event.target.value)}
+             />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Search
